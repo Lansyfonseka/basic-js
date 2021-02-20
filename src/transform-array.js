918e0026d10d1a4fb3c17877b09012b8df31d16c
lansyfonseka <twinkstriker@gmail.com>
2021-02-20 21:30:47 +0300

    solution transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('Not array');
+  }
+  let answ = [...arr]
+  return [...arr].map ( (e,i,a) => {
+    switch (e){
+      case '--discard-next':
+        a[i+1] = undefined;
+        return;
+      case '--discard-prev':
+        return;
+      case '--double-next':
+        return a[i+1];
+      case '--double-prev':
+        return a[i-1];
+    }
+    if (a[i+1] == '--discard-prev') {
+      a[i+1] = undefined;
+      return
+    }
+    return e;
+  }).filter( e => e != undefined)  
 };
