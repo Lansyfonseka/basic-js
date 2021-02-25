ae10bdf1b9f5d518d91d20de5afe8c92c27fcc8d
lansyfonseka <twinkstriker@gmail.com>
2021-02-26 00:13:50 +0300

    solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(parametr){
+    this.reverse = parametr === false;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  encrypt(string, key) {
+    key = key.repeat(Math.ceil(string.length / key.length)).slice(0,string.length).toUpperCase();
+    string = string.toUpperCase();
+    let countOffset = 0;
+    string = string.split('').map( (e,i) => {
+      if (e.match('[A-Z]')) {
+        let indexOfString = this.alphabet.indexOf(e);
+        let indexOfKey = this.alphabet.indexOf(key[i-countOffset]);
+        let result = indexOfString+indexOfKey;
+        return this.alphabet[result > 25 ? result - 26 : result];
+      }
+      else {
+        countOffset++;
+        return e;
+      }
+    }).join('');;
+    return this.reverse == true ? this.invert(string) : string;
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(string, key) {
+    key = key.repeat(Math.ceil(string.length / key.length)).slice(0,string.length).toUpperCase();
+    string = string.toUpperCase();
+    let countOffset = 0;
+    string = string.split('').map( (e,i) => {
+      if (e.match('[A-Z]')) {
+        let indexOfString = this.alphabet.indexOf(e);
+        let indexOfKey = this.alphabet.indexOf(key[i-countOffset]);
+        let result = indexOfKey-indexOfString;
+        return this.alphabet[result < 1 ? -result : 26 - result];
+      }
+      else {
+        countOffset++;
+        return e;
+      }      
+    }).join('');
+    return this.reverse == true ? this.invert(string) : string;
+  }
+  invert(string){
+    return string.split('').reverse().join('');
   }
 }
 
