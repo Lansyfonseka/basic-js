52e14ec4e97b8830d418bda03be7cb445a3422ef
lansyfonseka <twinkstriker@gmail.com>
2021-02-28 19:44:16 +0300

    fix: carbon-dating

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,7 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, {repeatTimes, separator, addition, additionRepeatTimes, additionSeparator}) {
     
-  str = str + (addition !== undefined ? new Array(additionRepeatTimes).fill(addition+'').join(additionSeparator) : '');
+  str = str + (addition !== undefined ? new Array(additionRepeatTimes).fill(addition+'').join(additionSeparator ? additionSeparator : '|') : '');
   str = new Array(repeatTimes).fill(str).join( separator ? separator : '+');  
   
   return str;
